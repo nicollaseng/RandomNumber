729d3e26cf1ad7f5acb99e368373880185c803c0
Nicollas <n.matheuscarvalho@gmail.com>
2018-07-18 10:13:52 -0300

    First commit initializing app1

---
+++ index.ios.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import {
+  Text,
+  AppRegistry
+} from 'react-native'
+
+const App = () => {
+  return (
+  <Text>
+    Olá Mundo!
+  </Text>
+  )
+}
+
+AppRegistry.registerComponent('app1', () => App)
