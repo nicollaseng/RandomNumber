6a1bbfefb372f21391ebdd2e2685b13a36ed79c4
Nicollas <n.matheuscarvalho@gmail.com>
2018-07-18 22:46:02 -0300

    Finished app. initial version 1.0

--- index.ios.js
+++ index.ios.js
@@ -4,13 +4,16 @@ import {
   AppRegistry,
   View,
   Button,
-  StyleSheet
+  StyleSheet,
+  TouchableOpacity
+
 } from 'react-native'
 
 const estilo = {
-  text: {
+  texto: {
     fontSize:50,
-    backgroundColor: '#0099e6',
+    backgroundColor: '#53ff1a',
+    marginVertical: 300,
     color: 'white',
     padding: 20,
     textAlign: 'center',
@@ -18,11 +21,27 @@ const estilo = {
     shadowOffset: {width:0, height:10},
     shadowOpacity: 1,
     shadowRadius: 1,
-    marginVertical: 25
-  },
-  buton: {
-    height:100,
-  }
+    // fontStyle: 'italic',
+    fontWeight: 'bold'
+   },
+   estiloBotao: {
+     backgroundColor: "white",
+     padding:20,
+     shadowColor: "#53ff1a",
+     shadowOffset: {width:0, height:2},
+     shadowOpacity: 0.5,
+     borderColor: "#53ff1a",
+     borderWidth: 0.5,
+     borderRadius: 8
+     
+
+   },
+   textoBotao: {
+     fontSize: 30,
+     color: "#53ff1a",
+     fontWeight: 'bold',
+     textAlign: 'center'
+   }
 }
 
 const gerarNumeroAleatorio = () => {
@@ -32,15 +51,16 @@ const gerarNumeroAleatorio = () => {
 }
 
 const App = () => {
+  const {texto, estiloBotao, textoBotao} = estilo 
   return (
     <View>
       {alert('Olá seja bem-vindo ao Par ou Impar v1. Desenvolvedor: Nicollas Linhares')}
-      <Text style={estilo.text}>Par ou Impar</Text>
-      <Button
-        title="Gerar"
-        onPress={gerarNumeroAleatorio}
-        sytle={estilo.buton}
-       />
+      <Text style={texto}>Par ou Ímpar</Text>
+      <TouchableOpacity style={estiloBotao}>
+        <Text style={textoBotao}>Gerar</Text>
+      </TouchableOpacity>
+      {/* <Button title="Gerar" onPress={gerarNumeroAleatorio} color="#841584"/> */}
+
     </View>
   )
 }
